b4c125644add0ff1564628b6a66a4d5cd1095541
Fredrik-L <fredrik.liliequist@yh.nackademin.se>
2019-12-09 10:02:17 +0100

    Map and hero Finished

--- Dungeon.py
+++ Dungeon.py
@@ -1,14 +1,42 @@
-from array import *
 import os
-print("Welcome to MF.JFAM's Dungeon Run!")
-def print_map(grid, map_Size):
-    for i in range(map_Size):
-        for j in range(map_Size):
-            print(grid[i,j], end=" ")
-        print() 
 
+def game_meny(inpt):
+    """
+        Prints out a pretty meny with a input.
+    """
+    print("*********************************************************")
+    print(f"           {inpt}                                       ")
+    print("*********************************************************")
+
+def print_map(grid, map_Size):
+    """
+        Prints out the map with two loops.
+        Arguments are the grid(dict) and map size(int).
+    """
+    for x in range(map_Size):
+        for y in range(map_Size):
+            print(grid[x,y], end=" ")
+        print() 
+    print("\n")
+def create_map(map_Size):
+    """
+        Creates the map with 2 for-loops. 
+        It will be a dict where the keys are the x and y pos
+        and the value will be x.
+        Returns the Dict.
+    """
+    grid_map = {}
+    for x in range(map_Size):
+        for y in range(map_Size):
+            grid_map[(x,y)] = "x"
+    return grid_map
 
 def int_input(question):
+    """
+        Error-handling for int questions. Argument is a text string.
+        This only works for meny where you can only answer 1, 2 or 3.
+        Returns the right answer.
+    """
     while True:
         try:
             user_input = int(input(question))
@@ -22,6 +50,10 @@ def int_input(question):
             print("You make your choice by entering 1,2 or 3.")
             int_input(question)
 def accepted_Hero_Input(question):
+    """
+        Error-handling for y or n. 
+        Will return the answer.
+    """
     answer = str(input(question))
     answer = answer.lower()
     if answer == "y" or answer == "n":
@@ -29,28 +61,30 @@ def accepted_Hero_Input(question):
     else:
         print("Please enter y for yes or n for no.")
         accepted_Hero_Input(question)
-def create_map(map_Size):
-    grid_map = {}
-    for x in range(map_Size):
-        for y in range(map_Size):
-            grid_map[(x,y)] = "x"
-    return grid_map
+
+game_meny("Welcome to MF.JFAM's Dungeon Run!")
+#Userame will be linked to the game save.
+#This will be updated in the last sprint.
 username = str(input("What is your username?\n> "))
 
+#While loop for Hero Choice.
 while True:
     os.system("cls")
+    game_meny(f"Welcome, {username}!")
     hero_Choice = int_input("What hero do you wanna play?\n1.Knight\n2.Wizard\n3.Thief\n> ")
-    print("\n")
+
     if hero_Choice == 1:
         print("--Knight--\nInitiative 5\nDurability 9\nAttack 6\nFlexibility 4\n"
         "\nThe knight's special ability is Shieldblock. Avoid damage on the first attack")
         answer = accepted_Hero_Input("Do you wanna play with the Knight? y/n \n>")
         hero = "Knight"
+    
     if hero_Choice == 2:
         print("--Wizard--\nInitiative 6\nDurability 4\nAttack 9\nFlexibility 5\n"
         "\nThe wizard's special ability is Flash of Light. An 80% chance to escape from combat")
         answer = accepted_Hero_Input("Do you wanna play with the Wizard? y/n \n>")
         hero = "Wizard"
+    
     if hero_Choice == 3:
         print("--Thief--\nInitiative 7\nDurability 5\nAttack 5 \nFlexability 7\n"
         "\nSpecial Ability: Critical Hit. The thief has 25% chance to do double damage with each attack.")
@@ -58,9 +92,13 @@ while True:
         hero = "Thief"
     if answer == "y":
         break
+
 os.system("cls")
-print(f"Playing as {hero}!")
+game_meny(f"Playing as {hero}!")
+
 map_Choice = int_input("What map size do you wanna play?\n1.Small\n2.Medium\n3.Large\n> ")
+
+#Map Size will decide if it should be a 4x4, 5x5 or 8x8 map.
 
 if map_Choice == 1:
     map_Size = 4
@@ -69,21 +107,30 @@ if map_Choice == 2:
 if map_Choice == 3:
     map_Size = 8
 
+#Create the map and saves it to grid_map
 grid_map = create_map(map_Size)
 os.system("cls")
+game_meny(f"Playing as {hero}!")
+#While loop for 
 while True:
     right_input = ("1","2","3","4")
-    start_pos_input = str(input("What corner you wanna start in?\n1.Up left\n2.Up right\n3.Down left\n4.Down right\n> "))
+    
+    start_pos_input = str(input("What corner you wanna start in?\n1.Top left\n2.Top right\n3.Bottom left\n4.Bottom right\n> "))
+    
     if start_pos_input in right_input:
+        #Sets the start pos, 4 diffrent cases.
         if start_pos_input == "1":
             start_pos_x = 0
             start_pos_y = 0
+        
         if start_pos_input == "2":
             start_pos_x = 0
             start_pos_y = map_Size - 1
+        
         if start_pos_input == "3":
             start_pos_x = map_Size - 1
             start_pos_y = 0
+        
         if start_pos_input == "4":
             start_pos_x = map_Size - 1
             start_pos_y = map_Size - 1
@@ -92,40 +139,54 @@ while True:
     else:
         os.system("cls")
         print("You make your choice by entering 1, 2, 3 or 4")
+
+# @ Symbol is for the hero, sets it to the start pos.
 grid_map[start_pos_x,start_pos_y] = "@"
+#Hero pos variable is for to check where the hero is.
 hero_pos = [start_pos_x, start_pos_y]
+
+#While loop for movement.
+wrong_input = ""
 while True:
     os.system("cls")
+    print(wrong_input)
+    wrong_input = ""
     print_map(grid_map,map_Size)
+
+    #Open room will be an O.
     grid_map[hero_pos[0], hero_pos[1]] = "o"
-    movement = str(input("Where do you wanna go? Use W for up, A for left, D for right, S for down."))
+    movement = str(input("Where do you wanna go?"))
+    
     if movement == "w":
         hero_pos[0] = hero_pos[0] - 1
-    if movement == "s":
+    elif movement == "s":
         hero_pos[0] = hero_pos[0] + 1
-    if movement == "d":
+    elif movement == "d":
         hero_pos[1] = hero_pos[1] + 1
-    if movement == "a":
+    elif movement == "a":
         hero_pos[1] = hero_pos[1] - 1
-
-    if movement == "q":
+    elif movement == "q":
         break
+    else:
+        wrong_input = "Use W for up, A for left, D for right, S for down.\n"
+    
     #If the hero walk too far down.
     if hero_pos[0] > map_Size - 1:
-       print("You cant go that way!")
+       wrong_input = "You cant go that way!\n"
        hero_pos[0] = hero_pos[0] - 1
     #If the hero walk too far up.
     if hero_pos[0] < 0:
-        print("You cant go that way!")
+        wrong_input = "You cant go that way!\n"
         hero_pos[0] = hero_pos[0] + 1
     #If the hero walk too far right.
     if hero_pos[1] > map_Size - 1:
-        print("You cant go that way!")
+        wrong_input = "You cant go that way!\n"
         hero_pos[1] = hero_pos[1] - 1
     #If the hero walk too far left.
     if hero_pos[1] < 0:
-        print("You cant go that way!")
+        wrong_input = "You cant go that way!\n"
         hero_pos[1] = hero_pos[1] + 1
+    #Puts @ where the hero is.
     else:
         grid_map[hero_pos[0], hero_pos[1]] = "@"
         
